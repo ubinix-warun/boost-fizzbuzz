3bb57d65d6dc554fb47a2213a34fbdb4d3526c5a
ubinix-warun <warun@ubinix.com>
2016-03-12 00:11:19 +0700

    refactoring to 'FIZZBUZZ_NotDiv3or5ShouldReturnIt'

--- test_fizzbuzz.cpp
+++ test_fizzbuzz.cpp
@@ -2,24 +2,19 @@
 
 BOOST_AUTO_TEST_SUITE(FIZZBUZZ)
 
-BOOST_AUTO_TEST_CASE(FIZZBUZZ_1ShouldReturn1)
+BOOST_AUTO_TEST_CASE(FIZZBUZZ_NotDiv3or5ShouldReturnIt)
 {
     BOOST_ASSERT_MSG(fizzbuzz(1) == 1, "1=>1");
+    BOOST_ASSERT_MSG(fizzbuzz(2) == 2, "2=>2");
+    BOOST_ASSERT_MSG(fizzbuzz(4) == 4, "4=>4");
+    BOOST_ASSERT_MSG(fizzbuzz(7) == 7, "7=>7");
+    BOOST_ASSERT_MSG(fizzbuzz(8) == 8, "8=>8");
 }
 
-BOOST_AUTO_TEST_CASE(FIZZBUZZ_2ShouldReturn2)
-{
-    BOOST_ASSERT_MSG(fizzbuzz(2) == 2, "2=>2");
-}
 
 BOOST_AUTO_TEST_CASE(FIZZBUZZ_3ShouldReturnFizz)
 {
     BOOST_ASSERT_MSG(fizzbuzz(3) == -1, "3=>Fizz");
-}
-
-BOOST_AUTO_TEST_CASE(FIZZBUZZ_4ShouldReturn4)
-{
-    BOOST_ASSERT_MSG(fizzbuzz(4) == 4, "4=>4");
 }
 
 BOOST_AUTO_TEST_CASE(FIZZBUZZ_5ShouldReturnBuzz)
