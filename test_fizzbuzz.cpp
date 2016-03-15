7558b068c5d85708feaa28742988e0566aad0958
ubinix-warun <warun@ubinix.com>
2016-03-15 12:16:56 +0700

    add '11' case, FIZZBUZZ_Div3ShouldReturnFizz and FIZZBUZZ_Div5ShouldReturnBuzz

--- test_fizzbuzz.cpp
+++ test_fizzbuzz.cpp
@@ -9,31 +9,19 @@ BOOST_AUTO_TEST_CASE(FIZZBUZZ_NotDiv3or5ShouldReturnIt)
     BOOST_ASSERT_MSG(fizzbuzz(4) == 4, "4=>4");
     BOOST_ASSERT_MSG(fizzbuzz(7) == 7, "7=>7");
     BOOST_ASSERT_MSG(fizzbuzz(8) == 8, "8=>8");
+    BOOST_ASSERT_MSG(fizzbuzz(11) == 1, "1=>11");
 }
 
-
-BOOST_AUTO_TEST_CASE(FIZZBUZZ_3ShouldReturnFizz)
+BOOST_AUTO_TEST_CASE(FIZZBUZZ_Div3ShouldReturnFizz)
 {
     BOOST_ASSERT_MSG(fizzbuzz(3) == -1, "3=>Fizz");
-}
-
-BOOST_AUTO_TEST_CASE(FIZZBUZZ_5ShouldReturnBuzz)
-{
-    BOOST_ASSERT_MSG(fizzbuzz(5) == -2, "5=>Buzz");
-}
-
-BOOST_AUTO_TEST_CASE(FIZZBUZZ_6ShouldReturnFizz)
-{
     BOOST_ASSERT_MSG(fizzbuzz(6) == -1, "6=>Fizz");
-}
-
-BOOST_AUTO_TEST_CASE(FIZZBUZZ_9ShouldReturnFizz)
-{
     BOOST_ASSERT_MSG(fizzbuzz(9) == -1, "9=>Fizz");
 }
 
-BOOST_AUTO_TEST_CASE(FIZZBUZZ_10ShouldReturnBuzz)
+BOOST_AUTO_TEST_CASE(FIZZBUZZ_Div5ShouldReturnBuzz)
 {
+    BOOST_ASSERT_MSG(fizzbuzz(5) == -2, "5=>Buzz");
     BOOST_ASSERT_MSG(fizzbuzz(10) == -2, "10=>Buzz");
 }
 
